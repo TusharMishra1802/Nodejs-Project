5e218bad5a9c6d8c1914daa57799c4dd15643f88
unknown <tusharmishra1802@gmail.com>
2022-10-23 01:32:38 +0530

    initia-code-setup

---
+++ index.js
@@ -0,0 +1,8 @@
+require('custom-env').env('main')
+const express = require('express');
+const app = express();
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server started on port ${process.env.PORT}`)
+})
